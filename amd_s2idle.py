6c08202ba91136e9d209f6e6af74ea5842d5c6f3
Mario Limonciello <mario.limonciello@amd.com>
2025-04-17 15:30:17 -0500

    amd_s2idle: Split out WakeIRQ class to it's own file
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- amd_s2idle.py
+++ amd_s2idle.py
@@ -20,6 +20,7 @@ try:
     import amd_debug.failures
     from amd_debug.installer import Installer
     from amd_debug.kernel_log import get_kernel_log, SystemdLogger, DmesgLogger
+    from amd_debug.wake import WakeIRQ
     from amd_debug.common import (
         read_file,
         print_color,
@@ -168,82 +169,6 @@ def pm_debugging(func):
         return ret
 
     return runner
-
-
-class WakeIRQ:
-    """Class for wake IRQs"""
-
-    def __init__(self, num, context):
-        self.num = num
-        p = os.path.join("/", "sys", "kernel", "irq", str(num))
-        self.chip_name = read_file(os.path.join(p, "chip_name"))
-        self.actions = read_file(os.path.join(p, "actions"))
-        self.driver = ""
-        self.name = ""
-        wakeup = read_file(os.path.join(p, "wakeup"))
-
-        # This is an IRQ tied to _AEI
-        if self.chip_name == "amd_gpio":
-            hw_gpio = read_file(os.path.join(p, "hwirq"))
-            self.name = f"GPIO {hw_gpio}"
-        # legacy IRQs
-        elif "IR-IO-APIC" in self.chip_name:
-            if self.actions == "acpi":
-                self.name = "ACPI SCI"
-            elif self.actions == "i8042":
-                self.name = "PS/2 controller"
-            elif self.actions == "pinctrl_amd":
-                self.name = "GPIO Controller"
-            elif self.actions == "rtc0":
-                self.name = "RTC"
-            elif self.actions == "timer":
-                self.name = "Timer"
-            self.actions = ""
-        elif "PCI-MSI" in self.chip_name:
-            bdf = self.chip_name.split("-")[-1]
-            for dev in context.list_devices(subsystem="pci"):
-                if dev.device_path.endswith(bdf):
-                    vendor = dev.properties.get("ID_VENDOR_FROM_DATABASE")
-                    desc = dev.properties.get("ID_PCI_CLASS_FROM_DATABASE")
-                    if not desc:
-                        desc = dev.properties.get("ID_PCI_INTERFACE_FROM_DATABASE")
-                    name = dev.properties.get("PCI_SLOT_NAME")
-                    self.driver = dev.properties.get("DRIVER")
-                    self.name = f"{vendor} {desc} ({name})"
-                    break
-
-        # "might" look like an ACPI device, try to follow it
-        if not self.name and self.actions:
-            p = os.path.join("/", "sys", "bus", "acpi", "devices", self.actions)
-            if os.path.exists(p):
-                for directory in os.listdir(p):
-                    if "physical_node" not in directory:
-                        continue
-
-                    for root, _dirs, files in os.walk(
-                        os.path.join(p, directory), followlinks=True
-                    ):
-                        if "name" in files:
-                            self.name = read_file(os.path.join(root, "name"))
-                            t = os.path.join(root, "driver")
-                            if os.path.exists(t):
-                                self.driver = os.path.basename(os.readlink(t))
-                            break
-                    if self.name:
-                        break
-
-        # If the name isn't descriptive try to guess further
-        if self.driver and self.actions == self.name:
-            if self.driver == "i2c_hid_acpi":
-                self.name = f"{self.name} I2C HID device"
-
-        # check if it's disabled
-        if not self.name and wakeup == "disabled":
-            self.name = "Disabled interrupt"
-
-    def __str__(self):
-        actions = f" ({self.actions})" if self.actions else ""
-        return f"{self.name}{actions}"
 
 
 class S0i3Validator(AmdTool):
