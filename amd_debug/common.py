b3875083808dcead6ba26dae7973eda1d6bf4d75
Mario Limonciello <mario.limonciello@amd.com>
2025-04-18 00:02:30 -0500

    amd_ptate: fix wrong character for status
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- amd_debug/common.py
+++ amd_debug/common.py
@@ -49,7 +49,7 @@ def get_group_color(group) -> str:
         color = Colors.DEBUG
     elif any(mk in group for mk in ["❌", "👀"]):
         color = Colors.FAIL
-    elif any(mk in group for mk in ["✅", "🔋", "🐧", "💻", "○", "💤", "🥱"]):
+    elif any(mk in group for mk in ["✅", "🔋", "🐧", "💻", "○", "💤", "🥱", "🫆"]):
         color = Colors.OK
     else:
         color = group
