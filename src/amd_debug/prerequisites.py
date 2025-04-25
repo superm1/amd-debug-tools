5e4de2b7f2a41a719b22078d8c0803a6dd1dcc99
Mario Limonciello <mario.limonciello@amd.com>
2025-04-25 17:03:49 -0500

    s2idle: port ATA disk check fro master
    From commit 5bad9d8 ("amd_s2idle: Detect ATA devices correctly").
    
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- src/amd_debug/prerequisites.py
+++ src/amd_debug/prerequisites.py
@@ -227,7 +227,7 @@ class PrerequisiteValidator(AmdTool):
             has_ahci = True
             break
 
-        for dev in self.pyudev.list_devices(subsystem="ata", DRIVER="ahci"):
+        for dev in self.pyudev.list_devices(subsystem="block", ID_BUS="ata"):
             has_sata = True
             break
 
